5d3a9ac2b9d35cdf41a5730c047507bda531fd4c
Peng Wang <wangpeng@google.com>
2025-05-06 11:47:02 -0700

    Created lake project LeanSql under uexp/src/uexp/

---
+++ uexp/src/uexp/LeanSql.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `LeanSql` library.
+-- Import modules here that should be built as part of the library.
+import LeanSql.Basic
+import examples
---
+++ uexp/src/uexp/Main.lean
@@ -0,0 +1,4 @@
+import LeanSql
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
